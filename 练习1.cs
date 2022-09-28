8f11e8e25c44e0f6cadc20e423aa40c171874e7f
SHENYCN\92fighting <82627610@qq.com>
2022-09-29 04:00:55 +0800

    我的第一次练习

---
+++ 练习1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static 泛型约束.Program;
+
+namespace 泛型约束
+{
+    internal class Program
+    {
+        #region 练习题1
+        /*
+         * 单例模式 ：通过单例模式的方法创建的类在当前进程中只有一个实例
+         * 一是某个类只能有一个实例；二是它必须在内部创建实例；三是它必须自行向整个系统提供这个实例。
+           * 唯一性 全局性
+         */
+        #region 普通单例模式
+        public class Test1
+        {
+            private static Test1 test = new Test1();//私有
+            
+            public static Test1 Gettest //这个方法是用于外部调用得到实例的
+            {
+                get //设置只写属性
+                {
+
+                    return test;
+                }
+            }
+
+        }
+        public class Test6
+        {
+            private static Test6 test6 = null;//私有
+
+            public static Test6 Gettest //这个方法是用于外部调用得到实例的
+            {
+                get //设置只写属性
+                {
+                    if (test6 == null)
+                    {
+                        Console.WriteLine("Test6 Gettest"+DateTime.Now);
+                        test6 = new Test6();
+                    }    
+                    return test6;
+                }
+            }
+
+        }
+        #endregion
+        #region 用泛型实现一个单例模式基类
+        public class Test3<T> where T : new()
+        {
+            public int value = 55;
+            private static T Value = new T();
+            public static T Value1
+            {
+                get
+
+                { return Value; }
+            }
+        }
+
+        #endregion
+
+        //单例模式的使用
+        public class Test2 : Test3<Test2>
+        {
+            public int value = 55;
+            public static Test2 m = new Test2();
+        }
+
+        public class Test4 : Test3<Test4>
+        {
+            public static Test2 n = new Test2();
+            public static Test4 m = new Test4();
+        }
+
+        public class Teat5 : Test1
+            {
+            public static Teat5 m = new Teat5();
+            
+                } 
+            #endregion
+            
+        
+        
+        static void Main(string[] args)
+            {
+            Test2.Value1.value=10;
+            Console.WriteLine(Test2.Value1.value = 10);
+            Console.WriteLine(Test4.Value1.value = 55);
+
+            Test6 test6=Test6.Gettest;//结果是被实例化了一次
+
+
+            Console.ReadLine();
+            }
+        }
+    }
+
---
+++ 练习2.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 泛型约束2
+{
+    /*
+     * 利用泛型知识点，仿造ArrayList实现一个不确定数组类型的类
+     * 实现增删查改方法
+     */
+    internal class Program
+    {
+        #region 练习2
+        class ArrayList<T> 
+        {
+            public T[] array;
+
+            public int count;//当前存储的数
+            public ArrayList()
+            {
+                count = 0;
+                array = new T[30];
+            }
+            public void AddFun(T value)
+            {
+               if (count >= array.Length)
+                {
+                     T[] newArray = new T[Capacity*2];
+                    for(int i = 0; i < newArray.Length; i++)
+                    {
+                        newArray[i] = array[i];
+                    }
+                }
+               array[count] = value; 
+                count++;
+            }
+            //移除元素
+            public void RemoveFun(T value)
+            {
+               int indexs = -1;
+
+                for(int i = 0; i < count; i++)
+                {
+                    //不确定传进来的类型
+                    //知识点：Equals
+                    if (array[i].Equals(value))
+                    {
+                        indexs = i;
+                        break;
+                    }
+                    if (indexs != -1)
+                    {
+                        for (; indexs < count - 1; indexs--)
+                        {
+                            //替换原来的数组
+                            array[i] = array[indexs+1];
+                        }
+                        array[count - 1] = default(T);//将最后一个元素设为空房间
+                        
+                        count--;
+                    }
+                }
+
+            }
+
+
+            public void RemoveAt(int index)
+            {
+                if (index < 0 || index >= Count)
+                {
+                    Console.WriteLine("索引不合法");
+                    return;
+                }
+
+                {
+                    for (; index < count - 1; index++)
+                    {
+                        //替换原来的数组
+                        array[index] = array[index + 1];
+                    }
+                    array[count - 1] = default(T);//将最后一个元素设为空房间
+
+                    count--;
+                }
+            }
+            public T this[int index]
+            {
+                get {
+                    if (index < 0 || index >= Count)
+                    {
+                        Console.WriteLine("索引不合法");
+                        return default(T);
+                    }
+                    return array[index]; }
+                set { 
+                     if (index < 0 || index >= Count)
+                {
+                    Console.WriteLine("索引不合法");
+                    return;
+                }
+                    array[index] = value; }
+            }
+
+
+              public int  Capacity //获取容量
+            {
+                get { return array.Length; }
+            }
+              public int Count //得到具体存储多少值
+                { get { return count; } }
+               }
+        #endregion
+
+        static void Main(string[] args)
+        {
+
+            ArrayList <int> array = new ArrayList<int>();
+            Console.WriteLine(array.Count);
+            Console.WriteLine(array.Capacity);
+            Console.WriteLine("-------------");
+            array.AddFun(15);
+            array.AddFun(20);
+            array.AddFun(25);
+            array.AddFun(35);
+            array.AddFun(39);
+            array.RemoveFun(10);
+            Console.WriteLine(array.Count);
+            Console.WriteLine(array.Capacity);
+            Console.WriteLine("-------------");
+            Console.WriteLine(array[3]);
+            array.RemoveAt(2);
+            Console.WriteLine(array.Count);
+
+            ArrayList<string> array1 = new ArrayList<string>();
+
+            Console.ReadLine();
+        }
+    }
+}
